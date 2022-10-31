29241dcf14f41380e462e4faad0b094e0086c00a
Monalisa123M <117096353+Monalisa123M@users.noreply.github.com>
2022-10-31 20:11:14 +0530

    Add files via upload

---
+++ ATMExample.java
@@ -0,0 +1,53 @@
+package atm1;
+import java.util.Scanner;
+public class ATMExample {
+
+	public static void main(String[] args) {
+		int balance=100000,withdraw,deposite;
+Scanner sc=new Scanner(System.in);
+while(true)
+{
+	System.out.println("Automated Teller Machine");
+	System.out.println("Choose 1 for Withdraw");
+	System.out.println("Choose 2 for Deposite");
+	System.out.println("Choose 3 for Check Balance");
+	System.out.println("Choose 4 for Exit");
+	System.out.println("Choose the operation you want to perform:");
+	int choice=sc.nextInt();
+	switch(choice)
+	{
+	case 1:
+	
+	System.out.println("Enter money to be withdraw:");
+	withdraw=sc.nextInt();
+	if(balance>=withdraw){
+		balance= balance-withdraw;
+		System.out.println("Please collect your money");
+	}
+	else{
+		System.out.println("Insufficient Balance");
+	}
+	System.out.println("");
+	break;
+	
+	case 2:
+		System.out.println("Enter money to be deposited:");
+	deposite=sc.nextInt();
+	balance= balance+deposite;
+	System.out.println("Your Money has been successfully deposited");
+	System.out.println("");
+	break;
+	
+	case 3:
+		System.out.println("Balance:"+balance);
+		System.out.println("");
+		break;
+		
+		case 4:
+			System.exit(0);
+			
+}
+}
+	}
+
+}
